b3862cf6657f22c44e480ae34b62d054c6d03c92
Alexander Kind <kind@shineblu.com>
2023-04-18 18:24:50 +0000

    Stable build 18042023

--- bigbluebutton-html5/imports/ui/components/actions-bar/presentation-options/styles.js
+++ bigbluebutton-html5/imports/ui/components/actions-bar/presentation-options/styles.js
@@ -3,7 +3,9 @@ import Button from '/imports/ui/components/common/button/component';
 import { colorWhite } from '/imports/ui/stylesheets/styled-components/palette';
 
 const RestorePresentationButton = styled(Button)`
-  ${({ ghost }) => ghost && `
+    display: none;
+
+    ${({ ghost }) => ghost && `
       span {
         box-shadow: none;
         background-color: transparent !important;
